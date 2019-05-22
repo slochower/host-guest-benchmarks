5f0a5af1a9c50f18e43a9e3db0f2c839ce616124
David Slochower <slochower@gmail.com>
2019-05-21 17:59:50 -0700

    Vesper → Taproom

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-Vesper
+Taproom
 A library of host-guest systems to benchmark Open Force Field Consortium force fields.
 This module provides entry points for pAPRika.
 """
@@ -7,7 +7,7 @@ This module provides entry points for pAPRika.
 from setuptools import setup
 
 setup(
-    name="vesper",
+    name="taproom",
     entry_points={
         "benchmarks": [
             "host_guest_pairs = entry_points:host_guest_pairs"
