c91645f60866795a93ec3f78dee221c619f07e17
David Slochower <slochower@gmail.com>
2019-05-24 17:58:17 -0700

    More robust paths, add structures

--- taproom/entry_points.py
+++ taproom/entry_points.py
@@ -3,6 +3,7 @@ This module contains the function that generates the entry point used by pAPRika
 """
 
 from pathlib import Path
+import yaml
 import pkg_resources
 import logging
 
@@ -22,11 +23,11 @@ def find_hosts(directory):
         The list of directory paths containing YAML recipes.
     """
 
-    hosts = []
+    hosts = {}
     p = Path(directory).joinpath("systems")
-    logger.debug(f"Looking for hosts in {p.resolve()}...")
     for file in p.resolve().glob("*/*.yaml"):
-        hosts.append(file.resolve())
+        
+        hosts[file.parent.stem] = file.resolve()
         logger.debug(f"Found host: {file.resolve()}")
     return hosts
 
@@ -45,18 +46,25 @@ def find_guests(host):
         The list of directory paths containing YAML recipes.
     """
 
-    guests = []
+    guests = {}
     for file in Path(host).parents[0].glob("*/*.yaml"):
-        guests.append(file.resolve())
+        guests[file.parent.stem] = file.resolve()
         logger.debug(f"Found guest: {file.resolve()}")
     return guests
 
 
+def find_host_guest_pairs():
 
-host_guest_pairs = {}
+    host_guest_pairs = {}
 
-installed_module_location = Path(pkg_resources.resource_filename("taproom", "entry_points.py")).parents[0]
-hosts = find_hosts(installed_module_location)
-for host in hosts:
-    guests = find_guests(host)
-    host_guest_pairs[host] = guests
+    installed_module_location = Path(pkg_resources.resource_filename("taproom", "entry_points.py")).parents[0]
+    hosts = find_hosts(installed_module_location)
+    for host, host_path in hosts.items():
+        host_guest_pairs[host] = {}
+        host_guest_pairs[host]["yaml"] = host_path
+        guests = find_guests(host_path)
+        for guest, guest_path in guests.items():
+            host_guest_pairs[host][guest] = guest_path
+    return host_guest_pairs
+
+host_guest_pairs = find_host_guest_pairs()
