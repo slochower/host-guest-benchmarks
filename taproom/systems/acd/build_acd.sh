21be581b5e246f3e9383565d9af07126a1b715b4
David Slochower <slochower@gmail.com>
2019-07-24 16:24:18 -0700

    Add build scripts and structures for hosts

---
+++ taproom/systems/acd/build_acd.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+antechamber -fi mol2 -i acd.gaff.mol2 -fo mol2 -o acd.mol2 -at sybyl
---
+++ taproom/systems/bcd/build_bcd.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+antechamber -fi mol2 -i bcd.gaff.mol2 -fo mol2 -o bcd.mol2 -at sybyl
